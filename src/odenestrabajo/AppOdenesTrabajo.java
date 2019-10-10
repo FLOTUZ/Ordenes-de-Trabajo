771375348b638e16e11f925ba2affc15068c0959
Emmanuel Esquivel <flotuz10@gmail.com>
2019-10-10 10:25:43 -0500

    Se crea el proyecto

---
+++ src/odenestrabajo/AppOdenesTrabajo.java
@@ -0,0 +1,12 @@
+package odenestrabajo;
+
+public class AppOdenesTrabajo {
+
+    public static void main(String[] args) {
+        VentanaPadre v = new VentanaPadre();
+        v.setLocationRelativeTo(null);
+        v.setTitle("Ordenes de trabajo");
+        v.setVisible(true);
+    }
+    
+}
